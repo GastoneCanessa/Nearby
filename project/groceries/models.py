973e726e3ba2ff19f8c939a74424b9bcd71d80e3
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-05-22 09:36:32 +0200

    Set (Post,  Greengrocer) models
    I set  (Post, Greengrocer) prototype models.

---
+++ project/groceries/models.py
@@ -0,0 +1,41 @@
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+from django.contrib.gis.geos import fromstr
+from geopy.geocoders import Nominatim
+
+
+class Greengrocer(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    user = models.OneToOneField(User(), on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, unique=True)
+    tel = models.PositiveIntegerField(default=0)   
+    city = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    zip_code = models.PositiveIntegerField()
+    location = models.PointField()
+    
+    def __str__(self):
+        return self.name
+
+    def location(self):
+        geolocator = Nominatim(user_agent="nearby_greengrocer")
+        location = geolocator.geocode(f'{self.address}, {self.city}')
+        self.location = fromstr(
+            f'POINT({location.latitude} {location.longitude})', srid=4326
+            )
+
+    def save(self, *args, **kwargs):
+        self.location()
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+        
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    greengrocer = models.ForeignKey(Greengrocer, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
